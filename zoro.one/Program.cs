503f198859d5611ba7eed1745ac7ba0f629d7e46
lights <lightsever@hotmail.com>
2018-08-15 23:26:54 +0800

    add zoroone

---
+++ zoro.one/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zoro.one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            zoro.one.http.httpserver http = new one.http.httpserver();
+            http.Start(80,false);
+            while(true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ zoro.one/http/httpserver.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoro.one.http
+{
+    class httpserver
+    {
+        private IWebHost host;
+        public void Start(int port, bool useHttps)
+        {
+            host = new WebHostBuilder().UseKestrel((options) =>
+            {
+                options.Listen(IPAddress.Any, port, listenOptions =>
+                  {
+                      //if (!string.IsNullOrEmpty(sslCert))
+                      if (useHttps)
+                          listenOptions.UseHttps();
+                      //sslCert, password);
+                  });
+                options.Listen(IPAddress.Any, port+1, listenOptions =>
+                {
+                    //if (!string.IsNullOrEmpty(sslCert))
+                    //if (useHttps)
+                        listenOptions.UseHttps("http/https.cer","");
+                    //sslCert, password);
+                });
+            }
+            )
+            .Configure(app =>
+            {
+                //app.UseResponseCompression();
+                app.Run(ProcessAsync);
+            })
+            //.ConfigureServices(services =>
+            //{
+            //    services.AddResponseCompression(options =>
+            //    {
+            //        // options.EnableForHttps = false;
+            //        options.Providers.Add<GzipCompressionProvider>();
+            //        options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json-rpc" });
+            //    });
+
+            //    services.Configure<GzipCompressionProviderOptions>(options =>
+            //    {
+            //        options.Level = CompressionLevel.Fastest;
+            //    });
+            //})
+            .Build();
+
+            host.Start();
+        }
+        private async Task ProcessAsync(HttpContext context)
+        {
+        }
+    }
+}
