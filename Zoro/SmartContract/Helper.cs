f6dffb4152025c4e976948d3fe2560ca501a5681
Michael Yao <michaely2008@sina.com>
2018-12-20 13:34:09 +0800

    同步NEO的代码更新

--- Zoro/SmartContract/Helper.cs
+++ Zoro/SmartContract/Helper.cs
@@ -10,7 +10,7 @@ namespace Zoro.SmartContract
 {
     public static class Helper
     {
-        private static readonly ConcurrentDictionary<string, uint> method_hashes = new ConcurrentDictionary<string, uint>();
+        private static readonly ConcurrentDictionary<string, uint> MethodHashes = new ConcurrentDictionary<string, uint>();
 
         public static bool IsMultiSigContract(this byte[] script)
         {
@@ -75,11 +75,7 @@ namespace Zoro.SmartContract
 
         public static uint ToInteropMethodHash(this string method)
         {
-            if (method_hashes.TryGetValue(method, out uint hash))
-                return hash;
-            hash = BitConverter.ToUInt32(Encoding.ASCII.GetBytes(method).Sha256(), 0);
-            method_hashes[method] = hash;
-            return hash;
+            return MethodHashes.GetOrAdd(method, p => BitConverter.ToUInt32(Encoding.ASCII.GetBytes(p).Sha256(), 0));
         }
 
         public static UInt160 ToScriptHash(this byte[] script)
