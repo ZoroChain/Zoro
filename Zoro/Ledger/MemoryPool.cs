bba94d0efabc373b0f592876aeff7187f0f99851
Michael Yao <11@11.com>
2019-01-10 18:16:52 +0800

    remove lowest fee when transaction pool is fulled

--- Zoro/Ledger/MemoryPool.cs
+++ Zoro/Ledger/MemoryPool.cs
@@ -1,5 +1,4 @@
 ﻿using Zoro.Network.P2P.Payloads;
-using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -9,7 +8,15 @@ namespace Zoro.Ledger
 {
     internal class MemoryPool : IReadOnlyCollection<Transaction>
     {
+        internal class Index
+        {
+            public UInt256 Hash;
+            public Fixed8 FeeRatio;
+            public Fixed8 Fee;
+        };
+
         private readonly ConcurrentDictionary<UInt256, Transaction> _mem_pool = new ConcurrentDictionary<UInt256, Transaction>();
+        private readonly List<Index> _index_list = new List<Index>();
 
         public int Capacity { get; }
         public int Count => _mem_pool.Count;
@@ -37,10 +44,13 @@ namespace Zoro.Ledger
         {
             if (Count > Capacity)
             {
-                return false;
+                if (!RemoveLowestFee(tx))
+                    return false;
             }
 
             _mem_pool.TryAdd(hash, tx);
+
+            AddIndex(hash, tx);
 
             return true;
         }
@@ -49,6 +59,7 @@ namespace Zoro.Ledger
         {
             if (_mem_pool.TryRemove(hash, out tx))
             {
+                RemoveIndex(hash);
                 return true;
             }
             else
@@ -81,6 +92,44 @@ namespace Zoro.Ledger
             {
                 return _mem_pool.Values.ToArray();
             }
+        }
+
+        private void AddIndex(UInt256 hash, Transaction tx)
+        {
+            _index_list.Add(new Index { Hash = hash, Fee = tx.SystemFee, FeeRatio = GetFeeRatio(tx) });
+
+            _index_list.OrderBy(p => p.FeeRatio).ThenBy(p => p.Fee);
+        }
+
+        private void RemoveIndex(UInt256 hash)
+        {
+            foreach (var index in _index_list)
+            {
+                if (index.Hash.Equals(hash))
+                {
+                    _index_list.Remove(index);
+                    break;
+                }
+            }
+        }
+
+        private Fixed8 GetFeeRatio(Transaction tx)
+        {
+            return tx.SystemFee / tx.Size;
+        }
+
+        private bool RemoveLowestFee(Transaction tx)
+        {
+            Index index = _index_list.First();
+
+            if (index.FeeRatio < GetFeeRatio(tx))
+            {
+                TryRemove(index.Hash, out Transaction _);
+                RemoveIndex(index.Hash);
+                return true;
+            }
+
+            return false;
         }
     }
 }
